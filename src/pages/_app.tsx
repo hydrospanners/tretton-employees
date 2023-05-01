8328b7e3584141be5da55adb2c3a52407a2a0736
hydrospanners <thanghil@gmail.com>
2023-05-02 00:38:59 +0200

    add theme and theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from "next/app";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/styles/theme.tsx
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: "1.5rem",
+      fontWeight: 200,
+    },
+    h2: {
+      fontSize: "1.2rem",
+      fontWeight: 500,
+    },
+    h3: {
+      fontSize: "1rem",
+      fontWeight: 700,
+    },
+    body1: {
+      fontWeight: 200,
+    },
+  },
+});
